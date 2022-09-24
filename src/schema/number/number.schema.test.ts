d85f0c8bdc05c5acc19b781f19f50ec1ed44c483
Lindell Carter <lindellcarternyc@gmail.com>
2022-09-23 22:29:09 -0400

    adds first implementation of number schema

---
+++ src/schema/number/number.schema.test.ts
@@ -0,0 +1,89 @@
+import { numberSchema } from './number.schema';
+
+describe(numberSchema, () => {
+  it('returns a schema', () => {
+    const num = numberSchema();
+    expect(typeof num.parse).toBe('function');
+  });
+
+  it('ensures a number was given', () => {
+    const num = numberSchema();
+
+    expect(num.parse(100)).toBe(100);
+
+    expect(() => num.parse(null)).toThrow('REQUIRED');
+  });
+
+  it('uses custom error messages', () => {
+    const required_number = numberSchema({
+      required_error: 'NOPE',
+      invalid_type_error: 'NAN',
+    });
+
+    expect(() => required_number.parse(null)).toThrow('NOPE');
+    expect(() => required_number.parse(true)).toThrow('NAN');
+  });
+
+  // Other validators
+  describe('gt:value', () => {
+    it('ensures x > value', () => {
+      const gt1 = numberSchema().gt(1);
+
+      expect(gt1.parse(2)).toBe(2);
+
+      expect(() => gt1.parse(1)).toThrow('Expected number > 1. Got 1.');
+    });
+  });
+
+  describe('gte:value', () => {
+    it('ensures x >= value', () => {
+      const gte = numberSchema().gte(1);
+
+      expect(gte.parse(1)).toBe(1);
+      expect(() => gte.parse(0)).toThrow('Expected number >= 1. Got 0.');
+    });
+  });
+
+  describe('min:value', () => {
+    it('is an alias for #gte', () => {
+      const min = numberSchema().min(1);
+
+      expect(min.parse(1)).toBe(1);
+      expect(() => min.parse(0)).toThrow('Expected number >= 1. Got 0.');
+    });
+  });
+
+  describe('lt:value', () => {
+    it('ensures x < value', () => {
+      const lt = numberSchema().lt(10);
+
+      expect(lt.parse(9)).toBe(9);
+
+      expect(() => lt.parse(10)).toThrow('Expected number < 10. Got 10.');
+    });
+  });
+
+  describe('lte:value', () => {
+    it('ensures x <= value', () => {
+      const lte = numberSchema().lte(10);
+
+      expect(lte.parse(10)).toBe(10);
+
+      expect(() => lte.parse(10.01)).toThrow(
+        'Expected number <= 10. Got 10.01.'
+      );
+    });
+  });
+
+  describe('max:value', () => {
+    it('is an alias for lte', () => {
+      const max = numberSchema().max(10);
+
+      expect(max.parse(10)).toBe(10);
+
+      expect(() => max.parse(10.01)).toThrow(
+        'Expected number <= 10. Got 10.01.'
+      );
+    });
+  });
+});
---
+++ src/schema/number/number.schema.ts
@@ -0,0 +1,92 @@
+import { ParserArgs, SchemaArgs, SchemaBuilder } from '../../lzod.types';
+import { BaseSchema } from '../base/base.schema';
+
+const isNumber = (x: unknown): x is number =>
+  typeof x === 'number' && !isNaN(x);
+
+class NumberSchema extends BaseSchema<number> {
+  #gt?: ParserArgs<number>;
+  #gte?: ParserArgs<number>;
+
+  #lt?: ParserArgs<number>;
+  #lte?: ParserArgs<number>;
+
+  constructor(args?: SchemaArgs) {
+    super(isNumber, args);
+  }
+
+  parse(value: unknown): number {
+    const numValue = super.parse(value);
+
+    if (this.#gt && numValue <= this.#gt.value) {
+      const message =
+        this.#gt.message ??
+        `Expected number > ${this.#gt.value}. Got ${numValue}.`;
+      throw new Error(message);
+    }
+
+    if (this.#gte && numValue < this.#gte.value) {
+      const message =
+        this.#gte.message ??
+        `Expected number >= ${this.#gte.value}. Got ${numValue}.`;
+      throw new Error(message);
+    }
+
+    if (this.#lt && numValue >= this.#lt.value) {
+      const message =
+        this.#lt.message ??
+        `Expected number < ${this.#lt.value}. Got ${numValue}.`;
+      throw new Error(message);
+    }
+
+    if (this.#lte && numValue > this.#lte.value) {
+      const message =
+        this.#lte.message ??
+        `Expected number <= ${this.#lte.value}. Got ${numValue}.`;
+      throw new Error(message);
+    }
+    return numValue;
+  }
+
+  gt: SchemaBuilder<number, number> = (value, opts) => {
+    this.#gt = {
+      value,
+      ...opts,
+    };
+    return this;
+  };
+
+  gte: SchemaBuilder<number, number> = (value, opts) => {
+    this.#gte = {
+      value,
+      ...opts,
+    };
+    return this;
+  };
+
+  min = this.gte;
+
+  lt: SchemaBuilder<number, number> = (value, opts) => {
+    this.#lt = {
+      value,
+      ...opts,
+    };
+
+    return this;
+  };
+
+  lte: SchemaBuilder<number, number> = (value, opts) => {
+    this.#lte = {
+      value,
+      ...opts,
+    };
+
+    return this;
+  };
+
+  max = this.lte;
+}
+
+export const numberSchema = (args?: SchemaArgs) => {
+  return new NumberSchema(args);
+};
